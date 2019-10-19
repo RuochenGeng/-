0e05d1305d9cdf2b0cc785bbc5d92be2ab942ae1
ruochen <gzy19960601@qq.com>
2019-10-19 19:20:15 +0800

    修改

--- alibaixiu/public/assets/js/posts.js
+++ alibaixiu/public/assets/js/posts.js
@@ -9,7 +9,9 @@ $.ajax({
 
 // 处理日期时间格式
 function formateDate(date) {
+
     // 将日期时间字符串转换成日期对象
-    date = new Date(date);
-    return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
+    // date = new Date(date);
+    // return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
+    return moment().format('YYYY-MM-DD');
 }
