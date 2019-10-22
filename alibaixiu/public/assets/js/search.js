0d00885b500108b1905c9daf8e5d5dbcc676079c
ruochen <gzy19960601@qq.com>
2019-10-22 21:39:33 +0800

    v3.11

--- alibaixiu/public/assets/js/search.js
+++ alibaixiu/public/assets/js/search.js
@@ -3,6 +3,8 @@ var key = getUrlParams('key');
 // 根据搜索关键字调用搜索接口 获取搜索结果
 $.get("/posts/search/" + key,
     function(response) {
+        console.log(response);
+
         var html = template('searchTpl', { data: response });
         $('#listBox').html(html);
     }
